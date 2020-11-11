0bdefb13999f96bd5558ecd13d9151fcd91b6a5a
Felipe Mafra <felipe.mafra@gmail.com>
2020-11-11 14:41:09 -0300

    Incluido os teste da CalculadoraDePesoPorAreaDeAtuacaoService.

--- StoneChallenge.Test/CalculadoraDeBonusServiceTest.cs
+++ StoneChallenge.Test/CalculadoraDeBonusServiceTest.cs
@@ -11,21 +11,8 @@ namespace StoneChallenge.Test
 {
     public class CalculadoraDeBonusServiceTest
     {
-        //private ICalculadoraDePesoService _calculadoraDePesoPorAreaDeAtuacaoService;
-        //private ICalculadoraDePesoService _calculadoraDePesoPorTempoDeAdmissaoService;
-        //private ICalculadoraDePesoService _calculadoraDePesoPorFaixaSalarial;
-        //private ICalculadoraDeBonusService _calculadoraDeBonusService;
-
-        //public CalculadoraDeBonusServiceTest()
-        //{
-        //    _calculadoraDePesoPorAreaDeAtuacaoService = new CalculadoraDePesoPorAreaDeAtuacaoService();
-        //    _calculadoraDePesoPorTempoDeAdmissaoService = new CalculadoraDePesoPorTempoDeAdmissaoService();
-        //    _calculadoraDePesoPorFaixaSalarial = new CalculadoraDePesoPorFaixaSalarial();
-        //    _calculadoraDeBonusService = new CalculadoraDeBonusService(_calculadoraDePesoPorAreaDeAtuacaoService, _calculadoraDePesoPorTempoDeAdmissaoService, _calculadoraDePesoPorFaixaSalarial);
-        //}
-
         [Fact]
-        public void CalculadoraDePesoPorTempoDeAdmissaoService()
+        public void CalculadoraDePesoPorTempoDeAdmissaoServiceTest()
         {
             var _calculadora = new CalculadoraDePesoPorTempoDeAdmissaoService();
 
@@ -41,9 +28,128 @@ namespace StoneChallenge.Test
             };
 
             Assert.Equal(1, _calculadora.Calcular(funcionarioComMenosDeUmAno));
+
             // Peso 2: Mais de 1 ano e menos de 3 anos;
+            var funcionarioComMaisDeUmAnoEMenosDeTres = new Funcionario
+            {
+                Id = 9999001,
+                Nome = "Alberto",
+                Cargo = "Dummy",
+                Departamento = Departamento.Financeiro,
+                SalarioBruto = new decimal(2000.00),
+                DataDeAdmissao = DateTime.Now.AddYears(-2)
+            };
+
+            Assert.Equal(2, _calculadora.Calcular(funcionarioComMaisDeUmAnoEMenosDeTres));
+
             // Peso 3: Acima de 3 anos e menos de 8 anos;
+            var funcionarioComMaisDeTresAnosEMenosDeOito = new Funcionario
+            {
+                Id = 9999001,
+                Nome = "Alberto",
+                Cargo = "Dummy",
+                Departamento = Departamento.Financeiro,
+                SalarioBruto = new decimal(2000.00),
+                DataDeAdmissao = DateTime.Now.AddYears(-5)
+            };
+
+            Assert.Equal(3, _calculadora.Calcular(funcionarioComMaisDeTresAnosEMenosDeOito));
+
             // Peso 5: Mais de 8 anos
+            var funcionarioComMaisDeOitoAnos = new Funcionario
+            {
+                Id = 9999001,
+                Nome = "Alberto",
+                Cargo = "Dummy",
+                Departamento = Departamento.Financeiro,
+                SalarioBruto = new decimal(2000.00),
+                DataDeAdmissao = DateTime.Now.AddYears(-9)
+            };
+
+            Assert.Equal(5, _calculadora.Calcular(funcionarioComMaisDeOitoAnos));
+        }
+
+        [Fact]
+        public void CalculadoraDePesoPorAreaDeAtuacaoServiceTest()
+        {
+            var _calculadora = new CalculadoraDePesoPorAreaDeAtuacaoService();
+
+            // Peso 1: Diretoria;
+            var funcionarioDaDiretoria = new Funcionario
+            {
+                Id = 9999001,
+                Nome = "Alberto",
+                Cargo = "Dummy",
+                Departamento = Departamento.Diretoria,
+                SalarioBruto = new decimal(2000.00),
+                DataDeAdmissao = DateTime.Now.AddMonths(-3)
+            };
+
+            Assert.Equal(1, _calculadora.Calcular(funcionarioDaDiretoria));
+
+            // Peso 2: Contabilidade, Financeiro, Tecnologia;
+            var funcionarioDaContabilidade = new Funcionario
+            {
+                Id = 9999001,
+                Nome = "Alberto",
+                Cargo = "Dummy",
+                Departamento = Departamento.Contabilidade,
+                SalarioBruto = new decimal(2000.00),
+                DataDeAdmissao = DateTime.Now.AddMonths(-3)
+            };
+
+            Assert.Equal(2, _calculadora.Calcular(funcionarioDaContabilidade));
+
+            var funcionarioDoFinanceito = new Funcionario
+            {
+                Id = 9999001,
+                Nome = "Alberto",
+                Cargo = "Dummy",
+                Departamento = Departamento.Financeiro,
+                SalarioBruto = new decimal(2000.00),
+                DataDeAdmissao = DateTime.Now.AddMonths(-3)
+            };
+
+            Assert.Equal(2, _calculadora.Calcular(funcionarioDoFinanceito));
+
+            var funcionarioDaTecnologia = new Funcionario
+            {
+                Id = 9999001,
+                Nome = "Alberto",
+                Cargo = "Dummy",
+                Departamento = Departamento.Tecnologia,
+                SalarioBruto = new decimal(2000.00),
+                DataDeAdmissao = DateTime.Now.AddMonths(-3)
+            };
+
+            Assert.Equal(2, _calculadora.Calcular(funcionarioDaTecnologia));
+
+
+            // Peso 3: Serviços Gerais;
+            var funcionarioDeServicosGerais = new Funcionario
+            {
+                Id = 9999001,
+                Nome = "Alberto",
+                Cargo = "Dummy",
+                Departamento = Departamento.ServicosGerais,
+                SalarioBruto = new decimal(2000.00),
+                DataDeAdmissao = DateTime.Now.AddMonths(-3)
+            };
+
+            Assert.Equal(3, _calculadora.Calcular(funcionarioDeServicosGerais));
+
+            // Peso 5: Relacionamento com o Cliente;
+            var funcionarioDeRelacionamentoComOCliente = new Funcionario
+            {
+                Id = 9999001,
+                Nome = "Alberto",
+                Cargo = "Dummy",
+                Departamento = Departamento.RelacionamentoComOCliente,
+                SalarioBruto = new decimal(2000.00),
+                DataDeAdmissao = DateTime.Now.AddMonths(-3)
+            };
+
+            Assert.Equal(5, _calculadora.Calcular(funcionarioDeRelacionamentoComOCliente));
         }
     }
 }
